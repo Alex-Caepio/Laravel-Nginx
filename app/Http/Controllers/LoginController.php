436bd077945984ca9c941919c17f780448336711
Alex-Caepio <mjbcaepio@gmail.com>
2022-02-16 18:16:00 +0300

    Added validation in requests and deleted API versioning

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,21 +5,16 @@ namespace App\Http\Controllers\Api\V1;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ValidateRegistration;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
-    public function register(Request $request)
+    public function register(ValidateRegistration $request)
     {
-        $validatedData = $request->validate([
-            'email'=>'required|email',
-            'password'=>'required|confirmed',
-            'password_confirmation'=>'required|same:password'
-        ]);
-
-        $user = User::create($validatedData);
+        $user = User::create($request->validated());
 
         $accessToken = $user->createToken('authToken')->accessToken;
 
